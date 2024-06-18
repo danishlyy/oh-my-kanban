e3e1df49983dbd27c477ec448991209939b04001
liyongyong <liyongyong@yingmi.cn>
2024-06-18 10:33:22 +0800

    feat: add kanban-board and kanban-colum class style

--- src/App.js
+++ src/App.js
@@ -7,18 +7,12 @@ function App() {
       <header className="App-header">
         <h1>我的看板</h1>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className='kanban-board'>
+        <section className='kanban-column'></section>
+        <section className='kanban-column'></section>
+        <section className='kanban-column'></section>
+      </main>
     </div>
   );
 }
